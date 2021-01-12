1800a98ef9f6829db8a60db3b2b1a65ae559adf6
kirankalyan5 <kiransinbox1@gmail.com>
2021-01-12 08:05:03 +0100

    Add commented code for demo

--- series-01/prefetch-future-navigation/public/script.js
+++ series-01/prefetch-future-navigation/public/script.js
@@ -1,2 +1,16 @@
 // Copyright 2018 Google LLC.
 // SPDX-License-Identifier: Apache-2.0
+
+
+function injectLinkPrefetchIn4g(url) {
+  if (window.navigator.connection.effectiveType === '4g') {
+    //generate link prefetch tag
+    const linkTag = document.createElement('link');
+    linkTag.rel = 'prefetch';
+    linkTag.href = url;
+    linkTag.as = 'document';
+
+    //inject tag in the head of the document
+    document.head.appendChild(linkTag);
+  }
+}
